5dac98e62398de667721a5ff5b80aa6e281b0cee
Sam <r0790203@student.thomasmore.be>
2020-12-19 14:42:28 +0100

    app

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$conn = new mysqli("mydb-service", "root", "redhat", "mydb");
+// Check connection
+if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+
+$sql = "SELECT name FROM user";
+$result = $conn->query($sql);
+
+
+
+
+if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+                echo $row['name']."<br>";
+        }
+} else {
+        echo "0 results";
+}
+$conn->close();
